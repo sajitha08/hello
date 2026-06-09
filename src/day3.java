14e498986411d5fafe76e8e6f8f2677b56e0dd36
sajitha <sajithac25aid@vetias.ac.in>
2026-06-09 11:57:08 +0530

    attendence

---
+++ src/day3.java
@@ -0,0 +1,55 @@
+/*void main(){
+    int n=5;
+    //row loop
+    for (int row =1;row <=n;row++) {
+     //column loop
+    for (int column =1;column <=row;column++) {
+        System.out.print(column);
+    }
+        System.out.println();
+    }
+}*/
+/*void main()
+{
+    int[] marks = {87,23,44,89,32} ;
+    System.out.print("pass marks : ");
+            for (int i=0;i<marks.length;i++)
+            {
+                if (marks[i] >= 40)
+                {
+                    System.out.print(marks[i] +" ");
+                }
+            }
+            System.out.println();
+    System.out.print("fail marks : ");
+            for(int i=0;i<marks.length;i++)
+            {
+                if(marks[i]<40)
+                {
+                    System.out.print(marks[i]+" ");
+                }
+            }
+}*/
+
+void main() {
+    int[] marks = {87, 23, 44, 89, 32};
+
+    System.out.print("Pass marks : ");
+    for (int i = 0; i < marks.length; i++) {
+        if (marks[i] >= 40) {
+            System.out.print(marks[i] + " ");
+        }
+    }
+    System.out.println();
+
+    System.out.print("Fail marks : ");
+    for (int i = 0; i < marks.length; i++) {
+        if (marks[i] < 40) {
+            System.out.print(marks[i] + " ");
+        }
+    }
+}
+
+
+
+
