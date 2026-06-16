4dba009b3d7711f4878efc3fd61b1ceb0af43935
sajitha <sajichandar@gmail.com>
2026-06-16 13:46:19 +0530

    arraylist

--- src/day10.java
+++ src/day10.java
@@ -1,5 +1,24 @@
-
-
+import java.util.ArrayList;
+public class day10 {
+    static void main() {
+        ArrayList<String> list= new ArrayList<String> ();
+        //String[] StudentsList={"Saji","Siva","Ram","Lasi","Seetha"};
+        //System.out.println(StudentsList.length);
+        list.add("Saji");
+        list.add("Siva");
+        list.add("Ram");
+        list.add("Lasi");
+        list.add("Seetha");
+        for (String s:  list){
+            System.out.println(s);
+        }
+        list.remove("Ram");
+        System.out.println();
+        for (String s:  list){
+            System.out.println(s);
+        }
+    }
+}
 
 
 
