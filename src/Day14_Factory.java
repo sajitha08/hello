794f749fe318be4a981c34f18e2ae961e46699de
sajitha <sajichandar@gmail.com>
2026-06-22 14:34:10 +0530

    Factory

---
+++ src/Day14_Factory.java
@@ -0,0 +1,33 @@
+public class Day14_Factory {
+    interface Notification{
+        void send (String to , String message);
+    }
+    class TextNotification implements Notification{
+        @Override
+
+            public void send (String to , String message){
+                System.out.println("Sending Text: " +message +",to" + to);
+            }
+
+    }
+        class AudioNotification implements Notification{
+            @Override
+            public void send(String to, String message) {
+                    System.out.println("Sending Audio : " +message+ ",to" + to);
+                }
+            }
+            static Notification
+        getNotification(Day14_Factory factory,String type){
+                return switch (type){
+                    case "Text"-> factory.new TextNotification();
+                    case "Audio"-> factory.new AudioNotification();
+                    default -> throw new IllegalArgumentException("Invalid notification type: "+type);
+                };
+            }
+            static void main() {
+                Day14_Factory factory = new Day14_Factory();
+
+                String type = "Text";
+                getNotification(factory, type).send("789456123","new text");
+            }
+        }
