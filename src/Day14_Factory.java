ae836a3a0e8018daded74ede9752801db39bf7fa
sajitha <sajichandar@gmail.com>
2026-06-23 09:48:42 +0530

    Stratrgy

--- src/Day14_Factory.java
+++ src/Day14_Factory.java
@@ -6,14 +6,16 @@ public class Day14_Factory {
         @Override
 
             public void send (String to , String message){
-                System.out.println("Sending Text: " +message +",to" + to);
+                System.out.println("Sending Text : " +message );
+            System.out.println("to : " + to);
             }
 
     }
         class AudioNotification implements Notification{
             @Override
             public void send(String to, String message) {
-                    System.out.println("Sending Audio : " +message+ ",to" + to);
+                    System.out.println("Sending Audio : " +message);
+                    System.out.println(" , to" + to);
                 }
             }
             static Notification
@@ -28,6 +30,6 @@ public class Day14_Factory {
                 Day14_Factory factory = new Day14_Factory();
 
                 String type = "Text";
-                getNotification(factory, type).send("789456123","new text");
+                getNotification(factory, type).send("789456123" , "new text");
             }
         }
