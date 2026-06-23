4eeace8a17d12b3e5f767ee96441c2dd7f475a52
sajitha <sajichandar@gmail.com>
2026-06-23 10:27:39 +0530

    thread

---
+++ src/Day15.java
@@ -0,0 +1,18 @@
+public class Day15 {
+    static void main() throws InterruptedException{
+        Runnable chefTask = () -> {
+            String[] items = {"Burger", "Momos", "Pizza"};
+            for (int i = 0; i < items.length; i++) {
+                System.out.println("Cooking Items : " + items[i]);
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+        Thread chefThread = new Thread(chefTask, "chef_thread");
+        chefThread.start();
+        chefThread.join();
+    }
+}
